097bbe21d3dda6c12c2bf0878726c31e343e1c52
nikkzzzyu <nikzzzlumaguemerjoy@gmail.com>
2024-10-04 21:36:05 +0800

    Created,Edited,and Finish the  Welcome to CRUDIFY

---
+++ app/src/main/java/com/example/crudifyapplication/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.crudifyapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // "Check Now" button
+        val checkNowButton: Button = findViewById(R.id.checkNowButton)
+
+        // Set click listener for the button
+        checkNowButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
